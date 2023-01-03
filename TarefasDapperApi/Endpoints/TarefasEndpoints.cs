a0a4f229761221d41876c002c25afd0f9ff7682c
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2023-01-03 10:29:41 -0300

    Crud is complete! All HTTP verbs are done

--- TarefasDapperApi/Endpoints/TarefasEndpoints.cs
+++ TarefasDapperApi/Endpoints/TarefasEndpoints.cs
@@ -26,6 +26,33 @@ namespace TarefasDapperApi.Endpoints
                 using var con = await connectionGetter();
                 return con.Get<Tarefa>(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound($"A tarefa de id: {id} não foi encontrada...");
             });
+
+            app.MapPost("/tarefas", async (GetConnection connectionGetter, Tarefa Tarefa) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Insert(Tarefa);
+                return Results.Created($"/tarefas/{id}", Tarefa);
+            });
+
+            app.MapPut("/tarefas", async (GetConnection connectionGetter, Tarefa Tarefa) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Update(Tarefa);
+                return Results.Ok(Tarefa);
+            });
+
+            app.MapDelete("/tarefas/{id}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+
+                var deleted = con.Get<Tarefa>(id);
+
+                if (deleted is null)
+                    return Results.NotFound();
+
+                con.Delete(deleted);
+                return Results.Ok(deleted);
+            });
         }
     }
 }
